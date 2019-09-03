9165ebf3820cac1751bf5a86d7af260e469af063
Vladimir Stovpovoy <v.stovpovoy@gmail.com>
2019-09-03 11:52:23 +0300

    some chenges

--- terraform/GTB/AWS_CentralConsole.tf
+++ terraform/GTB/AWS_CentralConsole.tf
@@ -11,29 +11,29 @@ resource "aws_instance" "central_console" {
  }
 
  resource "aws_security_group" "gtb_ports" {
-  name        = "allowed_ports"  # List of ports 
+  name        = "allowed_ports"          # List of ports 
   description = "This port should be allowed"
 
   ingress {
-    from_port   = 443            # Web Portal and agents connect to CC
+    from_port   = 443                    # Web Portal and agents connect to CC
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 17023          # For Fingerprinting
-    to_port     = 17023
+    from_port   = 17023                   # For Fingerprinting
+    to_port     = 17023   
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 3128           # For Proxy
+    from_port   = 3128                    # For Proxy
     to_port     = 3128
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 1122           # SSH access
+    from_port   = 1122                    # SSH access
     to_port     = 1122
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
